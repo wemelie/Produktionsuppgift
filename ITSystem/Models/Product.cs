565950f668f61c3256f11fd02871c2d475e15928
Emelie Wallström <emelie.wallstrom1@learnet.se>
2025-09-19 17:08:32 +0200

    info

--- ITSystem/Models/Product.cs
+++ ITSystem/Models/Product.cs
@@ -12,7 +12,7 @@ namespace ITSystem.Models
         public string Name { get; set; }
 
         public decimal Price { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } //Info om pordukter
         
     }
 }
